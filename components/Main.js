1653e4510e609c424149a54c4b13cba1a96c6a18
Paul Montoya <paulmontoya15@gmail.com>
2021-02-22 20:55:58 -0500

    header completado y diseño mejorado

--- components/Main.js
+++ components/Main.js
@@ -1,45 +1,46 @@
 import React from 'react';
 import Link from "next/link";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-/* import MainStyle from '../styles/Main'; */
 
 
 const Main = ({ data }) => {
 
   return (
     <div className="Main">
-      <div className="Main-container">
-        <Carousel>
-          {data.results.map((mydata, index) => {
-            return (
-              <Carousel.Item key={mydata.results}>
-                <img
-                className="d-block w-100"
-                src={mydata.image}
-                alt={mydata.name}
-                />
-                <Carousel.Caption>
-                  <h2>{mydata.name}</h2>
-                  <p>{mydata.species}</p>
-                  <p>{mydata.status}</p>
-{/*                   <span key={index}>
-                    <Link href="/character/id">
-                      <Button variant="info">Info</Button>{' '}
-                    </Link>
-                  </span> */}
-                </Carousel.Caption>
-              </Carousel.Item>
+      <Row>
+        <Col></Col>
+        <Col xs={6}>
+          <Carousel>
+            {data.results.map((mydata, index) => {
+              return (
+                <Carousel.Item key={mydata.results}>
+                  <img  
+                    className="d-block w-100"
+                    src={mydata.image}
+                    alt={mydata.name}
+                  />
+                  <Carousel.Caption>
+                    <h2>{mydata.name}</h2>
+                    <p>{mydata.species}</p>
+                    <p>{mydata.status}</p>
+    {/*                   <span key={index}>
+                      <Link href="/character/id">
+                        <Button variant="info">Info</Button>{' '}
+                      </Link>
+                    </span> */}
+                  </Carousel.Caption>
+                </Carousel.Item>
+              )}
             )}
-          )}
-        </Carousel>
-      </div>
-
-{/*       <style jsx HomeStyle>
-        {MainStyle}
-      </style> */}
-
+          </Carousel>
+        </Col>
+        <Col></Col>
+      </Row>
     </div>
 )};
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 /* components */
 import Main from '../components/Main';
+import Header from '../components/Header'
 /*  Hooks, Important this work with Experience only */
 import useGetData from '../hooks/useGetData';
 
@@ -10,8 +11,10 @@ const Home = () => {
   const data = useGetData();
   console.log(data);
 
+
   return (
     <div>
+      <Header />
       <Main 
         data={ data }
       />
